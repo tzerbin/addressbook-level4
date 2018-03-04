5300c3720689abc1512c39cd60ea87e10040b21d
Damien <damien_sim@hotmail.com>
2018-03-04 14:22:42 +0800

    8 spaces

--- src/main/java/seedu/address/ui/PersonCard.java
+++ src/main/java/seedu/address/ui/PersonCard.java
@@ -13,8 +13,8 @@ import seedu.address.model.person.Person;
 public class PersonCard extends UiPart<Region> {
 
     private static final String FXML = "PersonListCard.fxml";
-    private static final String[] TAG_COLORS = { "teal", "red", "purple", "blue", "orange", "brown",
-            "green", "pink", "black", "grey" };
+    private static final String[] TAG_COLORS = {"teal", "red", "purple", "blue", "orange", "brown",
+        "green", "pink", "black", "grey" };
 
     /**
      * Note: Certain keywords such as "location" and "resources" are reserved keywords in JavaFX.
