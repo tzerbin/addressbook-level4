0fe0085fd6953c9898c50c49dadac28611e926b7
Muruges <muruges95@hotmail.com>
2018-03-20 21:27:12 +0800

    made updates to User Guide

--- src/main/java/seedu/address/logic/commands/calendar/AddAppointmentCommand.java
+++ src/main/java/seedu/address/logic/commands/calendar/AddAppointmentCommand.java
@@ -1,6 +1,8 @@
 package seedu.address.logic.commands.calendar;
 
 import static java.util.Objects.requireNonNull;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_END_DATE;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_END_TIME;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_LOCATION;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_NAME;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_START_DATE;
@@ -25,12 +27,16 @@ public class AddAppointmentCommand extends Command {
             + PREFIX_NAME + "APPOINTMENT NAME "
             + PREFIX_START_TIME + "START TIME "
             + PREFIX_START_DATE + "START DATE "
-            + PREFIX_LOCATION + "LOCATION\n"
+            + PREFIX_LOCATION + "LOCATION "
+            + PREFIX_END_TIME + "END TIME "
+            + PREFIX_END_DATE + "END DATE\n"
             + "Example: " + COMMAND_WORD + " "
             + PREFIX_NAME + "Oscars 2018 "
             + PREFIX_START_TIME + "18:00 "
             + PREFIX_START_DATE + "23/04/2018 "
-            + PREFIX_LOCATION + "Hollywood";
+            + PREFIX_LOCATION + "Hollywood "
+            + PREFIX_END_TIME + "20:00 "
+            + PREFIX_END_DATE + "23/04/2018";
 
     public static final String MESSAGE_SUCCESS = "Added appointment successfully";
 
