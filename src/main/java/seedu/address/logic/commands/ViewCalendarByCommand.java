019460c09808770370934e235d02702d5d8a2332
WJY-norainu <wjy.norainu@gmail.com>
2018-03-24 01:13:52 +0800

    Add ViewCalendarByCommand#isCalendarViewPageDifferent() method. The method checks if the calendarViewPage from user input is the same as the current one. Now the operation only gets executed if these two are not the same, otherwise outputs an error message.

--- src/main/java/seedu/address/logic/commands/ViewCalendarByCommand.java
+++ src/main/java/seedu/address/logic/commands/ViewCalendarByCommand.java
@@ -1,5 +1,7 @@
 package seedu.address.logic.commands;
 
+import com.calendarfx.view.CalendarView;
+import com.calendarfx.view.page.PageBase;
 import seedu.address.commons.core.EventsCenter;
 import seedu.address.commons.events.ui.ChangeCalendarViewPageRequestEvent;
 import seedu.address.logic.commands.exceptions.CommandException;
@@ -17,8 +19,8 @@ public class ViewCalendarByCommand extends Command {
             + "Parameters: CALENDARVIEWPAGE (must be one of day, week, month, year. Not case-sensitive)\n"
             + "Example: " + COMMAND_WORD + " alice bob charlie";
 
-    public static final String MESSAGE_NO_CHANGE_IN_CALENDARVIEW = "Calender is currently in %1$s view";
-    public static final String MESSAGE_SUCCESS = "Opened calendar view: %1$s";
+    public static final String MESSAGE_NO_CHANGE_IN_CALENDARVIEW = "Calender is already in %1$s view";
+    public static final String MESSAGE_SUCCESS = "Switched to view calendar by %1$s";
 
     private final String calendarViewPage;
 
@@ -27,9 +29,33 @@ public class ViewCalendarByCommand extends Command {
     }
 
     @Override
-    public CommandResult execute() {
+    public CommandResult execute() throws CommandException {
+        if (!isCalendarViewPageDifferent()) {
+            throw new CommandException(String.format(MESSAGE_NO_CHANGE_IN_CALENDARVIEW, calendarViewPage));
+        }
         EventsCenter.getInstance().post(new ChangeCalendarViewPageRequestEvent(calendarViewPage));
         return new CommandResult(String.format(MESSAGE_SUCCESS, calendarViewPage));
+    }
+
+    /**
+     * @return true if user attempts to change calendar view to a different view
+     */
+    private boolean isCalendarViewPageDifferent() {
+        CalendarView calendarView = model.getCelebCalendarView();
+        PageBase currentCalendarViewPage = calendarView.getSelectedPage();
+        if (calendarViewPage.contentEquals("day") && currentCalendarViewPage == calendarView.getDayPage()) {
+            return false;
+        }
+        if (calendarViewPage.contentEquals("week") && currentCalendarViewPage == calendarView.getWeekPage()) {
+            return false;
+        }
+        if (calendarViewPage.contentEquals("month") && currentCalendarViewPage == calendarView.getMonthPage()) {
+            return false;
+        }
+        if (calendarViewPage.contentEquals("year") && currentCalendarViewPage == calendarView.getYearPage()) {
+            return false;
+        }
+        return true;
     }
 
     @Override
