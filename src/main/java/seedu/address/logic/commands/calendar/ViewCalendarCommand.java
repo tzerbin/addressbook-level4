fde8ca5dd1edcccb21120555f7b246b1ca59323d
WJY-norainu <wjy.norainu@gmail.com>
2018-04-01 15:28:20 +0800

    Update documentation of ViewCalendar to match with current implementation

--- src/main/java/seedu/address/logic/commands/calendar/ViewCalendarCommand.java
+++ src/main/java/seedu/address/logic/commands/calendar/ViewCalendarCommand.java
@@ -23,10 +23,10 @@ public class ViewCalendarCommand extends Command {
     public static final String COMMAND_WORD = "viewCalendar";
     public static final String COMMAND_ALIAS = "vc";
 
-    public static final String MESSAGE_USAGE = COMMAND_WORD + ": Displays the calendar of the celebrity specified."
-            + "The parameter is case-sensitive.\n"
-            + "Parameter: CELEBRITY_KEYWORD"
-            + "Example: " + COMMAND_WORD + " Jay Chou";
+    public static final String MESSAGE_USAGE = COMMAND_WORD + ": Displays the calendar of the celebrity"
+            + " identified by the index number used in the last person listing.\n"
+            + "Parameter: INDEX (must be a positive integer and the person at the index must be a celebrity)\n"
+            + "Example: " + COMMAND_WORD + " 6";
 
     public static final String MESSAGE_NO_CHANGE_IN_CALENDAR = "The calendar shown currently is already %1$s's";
     public static final String MESSAGE_SUCCESS = "Switched to show %1$s's calendar";
@@ -42,20 +42,18 @@ public class ViewCalendarCommand extends Command {
     @Override
     public CommandResult execute() throws CommandException {
         List<Person> lastShownList = model.getFilteredPersonList();
-
         if (targetIndex.getZeroBased() >= lastShownList.size()) {
             throw new CommandException(Messages.MESSAGE_INVALID_PERSON_DISPLAYED_INDEX);
         }
-        Person personToShowCalendar = lastShownList.get(targetIndex.getZeroBased());
 
+        Person personToShowCalendar = lastShownList.get(targetIndex.getZeroBased());
         // person at the given index is not a celebrity
         if (!personToShowCalendar.hasTag(CELEBRITY_TAG)) {
             throw new CommandException(MESSAGE_NOT_CELEBRITY);
         }
 
         Celebrity celebrityToShowCalendar = (Celebrity) personToShowCalendar;
-
-        // not in appointment list view and same celebrity
+        // the celebrity's calendar is currently being shown
         if (celebrityToShowCalendar == model.getCurrentCelebCalendarOwner()
                 && !model.getIsListingAppointments()) {
             throw new CommandException((String.format(MESSAGE_NO_CHANGE_IN_CALENDAR,
@@ -63,8 +61,7 @@ public class ViewCalendarCommand extends Command {
         }
 
         model.setCelebCalendarOwner(celebrityToShowCalendar);
-        EventsCenter.getInstance().post(new ChangeCalendarRequestEvent(celebrityToShowCalendar));
-
+        EventsCenter.getInstance().post(new ChangeCalendarRequestEvent(celebrityToShowCalendar.getCelebCalendar()));
         // if it's switching from appointment list view to calendar
         if (model.getIsListingAppointments()) {
             EventsCenter.getInstance().post(new ShowCalendarEvent());
