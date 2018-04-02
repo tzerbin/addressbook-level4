3ca49544c40a18754dea04e75b47d4d9b2b6f45a
WJY-norainu <wjy.norainu@gmail.com>
2018-04-03 01:56:34 +0800

    Add more tests for RemoveCommand and its parser

--- src/test/java/seedu/address/logic/commands/RemoveTagCommandTest.java
+++ src/test/java/seedu/address/logic/commands/RemoveTagCommandTest.java
@@ -2,6 +2,13 @@ package seedu.address.logic.commands;
 
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
+import static seedu.address.logic.commands.CommandTestUtil.assertCommandFailure;
+import static seedu.address.logic.commands.CommandTestUtil.assertCommandSuccess;
+import static seedu.address.logic.commands.CommandTestUtil.prepareRedoCommand;
+import static seedu.address.logic.commands.CommandTestUtil.prepareUndoCommand;
+import static seedu.address.logic.commands.RemoveTagCommand.MESSAGE_CANNOT_REMOVE_CELEBRITY_TAG;
+import static seedu.address.logic.commands.RemoveTagCommand.MESSAGE_TAG_NOT_FOUND;
+import static seedu.address.model.ModelManager.CELEBRITY_TAG;
 import static seedu.address.testutil.TypicalPersons.getTypicalAddressBook;
 import static seedu.address.testutil.TypicalTags.FRIENDS_TAG;
 import static seedu.address.testutil.TypicalTags.HUSBAND_TAG;
@@ -27,11 +34,62 @@ public class RemoveTagCommandTest {
     public ExpectedException thrown = ExpectedException.none();
 
     private Model model = new ModelManager(getTypicalAddressBook(), new UserPrefs());
+    private Tag nonExistingTag = new Tag("thisTagNameIsSuperLongAndThereShouldntBeAnyoneWithSuchATag");
 
     @Test
     public void constructor_nullTag_throwsNullPointerException() {
         thrown.expect(NullPointerException.class);
         new RemoveTagCommand(null);
+    }
+
+    @Test
+    public void execute_celebrityTag_throwsCommandException() {
+        RemoveTagCommand removeTagCommand = prepareCommand(CELEBRITY_TAG);
+        assertCommandFailure(removeTagCommand, model, MESSAGE_CANNOT_REMOVE_CELEBRITY_TAG);
+    }
+
+    @Test
+    public void execute_nonExistingTag_throwsCommandException() {
+        RemoveTagCommand removeTagCommand = prepareCommand(nonExistingTag);
+        assertCommandFailure(removeTagCommand, model,
+                String.format(MESSAGE_TAG_NOT_FOUND, nonExistingTag.toString()));
+    }
+
+    @Test
+    public void execute_friendsTag_success() throws Exception {
+        RemoveTagCommand removeTagCommand = prepareCommand(FRIENDS_TAG);
+        int count = model.countPersonsWithTag(FRIENDS_TAG);
+
+        String expectedMessage = String.format(
+                RemoveTagCommand.MESSAGE_DELETE_TAG_SUCCESS,
+                FRIENDS_TAG.toString(),
+                count);
+
+        Model expectedModel = new ModelManager(model.getAddressBook(), new UserPrefs());
+        expectedModel.removeTag(FRIENDS_TAG);
+
+        assertCommandSuccess(removeTagCommand, model, expectedMessage, expectedModel);
+    }
+
+    @Test
+    public void executeUndoRedo_friendsTagWhichThreePersonsHave_success() throws Exception {
+        Model expectedModel = new ModelManager(model.getAddressBook(), new UserPrefs());
+
+        UndoRedoStack undoRedoStack = new UndoRedoStack();
+        UndoCommand undoCommand = prepareUndoCommand(model, undoRedoStack);
+        RedoCommand redoCommand = prepareRedoCommand(model, undoRedoStack);
+        RemoveTagCommand removeTagCommand = prepareCommand(FRIENDS_TAG);
+
+        // removeTag -> friends tag removed
+        removeTagCommand.execute();
+        undoRedoStack.push(removeTagCommand);
+
+        // undo -> reverts address book back to previous state
+        assertCommandSuccess(undoCommand, model, UndoCommand.MESSAGE_SUCCESS, expectedModel);
+
+        // redo -> friends tag deleted again
+        expectedModel.removeTag(FRIENDS_TAG);
+        assertCommandSuccess(redoCommand, model, RedoCommand.MESSAGE_SUCCESS, expectedModel);
     }
 
     @Test
