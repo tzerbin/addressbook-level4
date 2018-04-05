0fa57bf8c3c181e40c8cdfa22ee5672f62eccd9c
Damien <damien_sim@hotmail.com>
2018-04-05 22:38:23 +0800

    resolve compilation error

--- src/main/java/seedu/address/logic/commands/calendar/ViewAppointmentCommand.java
+++ src/main/java/seedu/address/logic/commands/calendar/ViewAppointmentCommand.java
@@ -56,6 +56,6 @@ public class ViewAppointmentCommand extends Command {
                 + "End Time: " + selectedAppointment.getEndTime() + "\n"
                 + "Location: " + selectedAppointment.getLocation() + "\n"
                 + "Celebrities attending: " + selectedAppointment.getCelebritiesAttending() + "\n"
-                + "Points of Contact: " + selectedAppointment.getPointOfContacts();
+                + "Points of Contact: " + selectedAppointment.getPointsOfContact();
     }
 }
